18c4a074aeec1d8284e2db081e7a948869b53faa
Rob <whathehack81@gmail.com>
2026-06-27 09:11:46 -0400

    feat(rip): add validation layer package

---
+++ casper/rip/__init__.py
@@ -0,0 +1,11 @@
+from casper.rip.validation import (
+    ValidationAttempt,
+    ValidationCapsule,
+    run_validation,
+)
+
+__all__ = [
+    "ValidationAttempt",
+    "ValidationCapsule",
+    "run_validation",
+]
